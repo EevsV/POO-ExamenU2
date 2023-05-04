60f52db2d10deaaa7b0f8d0967c94a4dffc01956
MarioSylva03 <124391494+MarioSylva03@users.noreply.github.com>
2023-05-03 21:00:06 -0600

    Add files via upload

--- ExamenU2_Problema1/src/examenu2_problema1/ExamenU2_Problema1.java
+++ ExamenU2_Problema1/src/examenu2_problema1/ExamenU2_Problema1.java
@@ -16,6 +16,13 @@ public class ExamenU2_Problema1 {
      */
     public static void main(String[] args) {
         
+        //INTEGRANTES DEL EQUIPO
+        System.out.println("Fabian Armando Bustillos Vitela 22550344");
+        System.out.println("Sebastian Gardea Saucedo 22550363");
+        System.out.println("Isaac Manuel Guzman Gonzales 22550331");
+        System.out.println("Omar Alfonso Palomino Romero 22550333");
+        System.out.println("Mario Sylva Iracheta 22550369");
+        System.out.println("");
                              //VALOREZ DADOS EN ORDEN DEL CONSTRUCTOR CON ARGUMENTOS
                            //VF  VI  A   T                           
         D1 Formula1 = new D1(80, 20, 10, 30);
